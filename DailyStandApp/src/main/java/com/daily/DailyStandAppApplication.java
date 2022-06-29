c1af0d72715cac72cc48c2cf0d1ce82e51ac1dbe
Nicole Olivares <nicoleolivaresv@gmail.com>
2022-06-29 18:23:00 -0400

    Carpetas proyecto

---
+++ DailyStandApp/src/main/java/com/daily/DailyStandAppApplication.java
@@ -0,0 +1,13 @@
+package com.daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyStandAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyStandAppApplication.class, args);
+	}
+
+}
