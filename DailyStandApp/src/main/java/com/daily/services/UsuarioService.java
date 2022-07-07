f275cc32eec59e94f2c8c7e3dab4f6021f7a537c
EktorGG <profesorhectorg@gmail.com>
2022-07-06 21:40:23 -0400

    Formulario de registro de usuario jsp y otros

--- DailyStandApp/src/main/java/com/daily/services/UsuarioService.java
+++ DailyStandApp/src/main/java/com/daily/services/UsuarioService.java
@@ -1,8 +1,8 @@
 package com.daily.services;
-
 import java.util.List;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +17,10 @@ public class UsuarioService {
     // Inyeccion de dependencia usuario
     @Autowired
     UsuarioRepository usuarioRepository;
+
+    public void saveUsuario(@Valid Usuario usuario) {
+        usuarioRepository.save(usuario);
+    }
 
     public List<Usuario> findAll() {// metodo para obtener los usuarios
         return usuarioRepository.findAll();
