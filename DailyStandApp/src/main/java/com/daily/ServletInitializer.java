1136cb3e1206675511520274579e04ab608cbf31
Nicole Olivares <nicoleolivaresv@gmail.com>
2022-07-12 20:26:34 -0400

    commit de prueba

---
+++ DailyStandApp/src/main/java/com/daily/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.daily;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DailyStandAppApplication.class);
+	}
+
+}
